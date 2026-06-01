e8d26252112afde7a848e3bdfaf1d1825d99f178
Laurent Lacôte <laurent.lacote@gmail.com>
2026-06-01 10:05:00 +0200

    SQL - CRUD - Fix: T0 Dissonance with checker expectations
    1/ Checker expected all fields to be required,
         fair enough.
    2/ Checker expected type definitions to use
         official SQLite keywords, ok (although
         it makes script less portable).
    3/ Checker expected published year to be a
         plain integer instead of DATE... Bad.

--- sql_crud/0-create_table.sql
+++ sql_crud/0-create_table.sql
@@ -3,8 +3,8 @@ CREATE TABLE books (
     id INTEGER PRIMARY KEY AUTOINCREMENT, -- Obvious
     title TEXT NOT NULL,   -- Title should be required, TEXT because no idea of limit to apply
     author TEXT NOT NULL,  -- Same (no VARCHAR(x)) because no good idea of reasonable limit
-    genre TEXT,            -- Don't see why it should be mandatory
-    price FLOAT NOT NULL,  -- Consider this required in context of books shop
-    stock INT NOT NULL,    -- Same as above
-    published_year DATE   -- Not a mandatory information IMO
+    genre TEXT NOT NULL,            -- Don't see why it should be mandatory
+    price REAL NOT NULL,  -- Consider this required in context of books shop
+    stock INTEGER NOT NULL,    -- Same as above
+    published_year INTEGER NOT NULL   -- Not a mandatory information IMO
 );
