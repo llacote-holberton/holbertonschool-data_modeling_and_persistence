38d84c0244b75168ab2a6093ab68567a14f14298
Laurent Lacôte <laurent.lacote@gmail.com>
2026-06-01 20:30:40 +0200

    SQL - J&R - Feat: T8 step 6: listing heavy load courses
    Aka courses with more students than the average amount.
    
    To try please run
    sqlite3 academy.db < 13-courses-above-average-assignments.sql

---
+++ sql_join/13-courses-above-average-assignments.sql
@@ -0,0 +1,43 @@
+WITH assignations_per_course AS 
+(
+  SELECT
+    course_id, -- REQUIRED FOR GROUP BY AND JOIN
+    count(student_id) as students_count
+  FROM registrations
+  GROUP BY course_id
+)
+
+SELECT courses.title
+FROM courses
+JOIN assignations_per_course
+  ON assignations_per_course.course_id = courses.id
+WHERE assignations_per_course.students_count > 
+  (
+      SELECT AVG(assignations_per_course.students_count) as average_count
+      FROM assignations_per_course
+  )
+
+ORDER BY courses.title ASC
+;
+
+
+/*
+ * ========== T8.6 Instructions ==========
+ *
+ * === BUSINESS REQUEST ===
+ * The curriculum team wants to identify courses that have a heavier-than-average assignment load.
+ * They define this as:
+ * - courses whose number of assignments is greater than the average number of assignments per course in the dataset
+ * The result must show:
+ * - the course title
+ * The list must be presented in alphabetical order by course title.
+ *
+ * === REQUEST ANALYSIS AND DECOMPOSITION ===
+ * To fulfill the request we need to...
+ * a) Get the count of assignations for each course
+ * b) Get the average across all courses
+ * c) Combine both to filter one compared to the other
+ *
+ * === NOTES ===
+ * Support of WITH keyword is very old (3.8.3) so I hope checker version will support it.
+ */
